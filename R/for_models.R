5c13c140f8602f79198a5af1c6b1fa5aa6407ddf
Carys Jones <carys.jones@wolfson.ox.ac.uk>
2023-12-08 13:12:08 +0000

    load pathway for functions

--- R/for_models.R
+++ R/for_models.R
@@ -4,6 +4,9 @@
 
 # DEPENDENCIES ─────────────────────────────────────────────────────────────── #
 
+
+box::use(mods = R / for_models)
+
 box::use(magrittr[`%>%`])
 box::use(dplyr)
 box::use(rptR)
@@ -18,12 +21,13 @@ box::use(rptR)
 #' @return dataset with certain/all years.
 
 
-choose_data <- function(data, years){
-  ifelse(years == 'ALL',
-         output <- data,
-         output <- data %>%
-           dplyr::filter(breeding_year %in% years) %>%
-           droplevels())
+choose_data <- function(data, years) {
+  ifelse(years == "ALL",
+    output <- data,
+    output <- data %>%
+      dplyr::filter(breeding_year %in% years) %>%
+      droplevels()
+  )
   return(output)
 }
 
@@ -37,26 +41,27 @@ choose_data <- function(data, years){
 get_var_comps <- function(model) {
   mod_name <- deparse(substitute(model))
 
-  var_comp_names <- switch(
-    mod_name,
-    LD_basic = c('Vby', 'Va', 'Vpe', 'Vr'),
-    HD_basic = c('Vby', 'Va', 'Vpe', 'Vr'),
-    LD_NB = c('Vby', 'Vnb', 'Va', 'Vpe', 'Vr'),
-    HD_NB = c('Vby', 'Vnb', 'Va', 'Vpe', 'Vr'),
-    LD_spat = c('Vby', 'Vspat', 'Va', 'Vpe', 'Vr'),
-    HD_spat = c('Vby', 'Vspat', 'Va', 'Vpe', 'Vr'),
-    LD_envir = c('Vby', 'Venv', 'Va', 'Vpe', 'Vr'),
-    HD_envir = c('Vby', 'Venv', 'Va', 'Vpe', 'Vr'),
+  var_comp_names <- switch(mod_name,
+    LD_basic = c("Vby", "Va", "Vpe", "Vr"),
+    HD_basic = c("Vby", "Va", "Vpe", "Vr"),
+    LD_NB = c("Vby", "Vnb", "Va", "Vpe", "Vr"),
+    HD_NB = c("Vby", "Vnb", "Va", "Vpe", "Vr"),
+    LD_spat = c("Vby", "Vspat", "Va", "Vpe", "Vr"),
+    HD_spat = c("Vby", "Vspat", "Va", "Vpe", "Vr"),
+    LD_envir = c("Vby", "Venv", "Va", "Vpe", "Vr"),
+    HD_envir = c("Vby", "Venv", "Va", "Vpe", "Vr"),
     stop("Invalid model specified.")
   )
 
   data <- summary(model)$varcomp %>%
     tibble::rownames_to_column(var = "name") %>%
-    base::replace(., 'name', var_comp_names) %>%
-    dplyr::rename('Est' = 'component', 'SE' = 'std.error') %>%
-    dplyr::mutate(Rel_Est = Est / sum(Est),
-                  Rel_SE = (SE / Est) * Rel_Est) %>%
-    dplyr::select(-c('z.ratio', 'bound', '%ch')) %>%
+    base::replace(., "name", var_comp_names) %>%
+    dplyr::rename("Est" = "component", "SE" = "std.error") %>%
+    dplyr::mutate(
+      Rel_Est = Est / sum(Est),
+      Rel_SE = (SE / Est) * Rel_Est
+    ) %>%
+    dplyr::select(-c("z.ratio", "bound", "%ch")) %>%
     dplyr::mutate(model_name = as.character(mod_name))
 
   return(data)
@@ -72,17 +77,16 @@ get_var_comps <- function(model) {
 #'
 
 get_herit_asreml <- function(data, model, modelname) {
-
   filter_by_model <- subset(data, model_name == modelname)
 
   # Calculate total phenotypic variance (Vp)
   Vp <- sum(filter_by_model$Est)
   # Calculate within-year phenotypic variance (Vp_yr)
-  Vp_yr <- sum(filter_by_model$Est[filter_by_model$name != 'Vby'])
+  Vp_yr <- sum(filter_by_model$Est[filter_by_model$name != "Vby"])
   # Calculate heritability across years (h2)
   # h2 <- subset(filter_by_model, name == 'Va')[, 2] / Vp
   # Calculate heritability within year (h2_yr)
-  h2_yr <- subset(filter_by_model, name == 'Va')[, 2] / Vp_yr
+  h2_yr <- subset(filter_by_model, name == "Va")[, 2] / Vp_yr
 
   # Choose the appropriate formula based on the number of rows
   if (nrow(filter_by_model) == 4) {
@@ -95,9 +99,9 @@ get_herit_asreml <- function(data, model, modelname) {
 
   # Create a tibble with results
   herits <- tibble::tibble(
-    name = c('Vp', 'h2_yr'),
+    name = c("Vp", "h2_yr"),
     Est = c(Vp, h2_yr),
-    SE = c(NA,  SE_h2_yr)
+    SE = c(NA, SE_h2_yr)
   )
 
   return(herits)
@@ -114,11 +118,13 @@ bbox_polygon_G <- function(breeding_data) {
   bb <- sf::st_bbox(breeding_data)
 
   p <- matrix(
-    c(bb["xmin"], bb["ymin"],
+    c(
+      bb["xmin"], bb["ymin"],
       bb["xmin"], bb["ymax"],
       bb["xmax"], bb["ymax"],
       bb["xmax"], bb["ymin"],
-      bb["xmin"], bb["ymin"]),
+      bb["xmin"], bb["ymin"]
+    ),
     ncol = 2, byrow = T
   )
 
@@ -135,37 +141,35 @@ bbox_polygon_G <- function(breeding_data) {
 #' @return dataset with areas for each box occupied within that year
 
 get_territory_polygons <- function(breeding_data, wood_outline, yr) {
-
-  #create box
+  # create box
   box <- sf::st_sfc(mods$bbox_polygon_G(breeding_data))
 
-  #find areas
+  # find areas
   breeding_data_areas <- breeding_data %>%
-    dplyr::filter(if_any(any_of(c("year", "breeding_year")), ~. == yr)) %>%
-      #get voronoi polygons
+    dplyr::filter(if_any(any_of(c("year", "breeding_year")), ~ . == yr)) %>%
+    # get voronoi polygons
     sf::st_union() %>%
     sf::st_voronoi(box) %>%
     sf::st_cast() %>%
     sf::st_intersection(sf::st_union(wood_outline)) %>%
-    #joining the territory polygons back up with the individuals that bred in them
+    # joining the territory polygons back up with the individuals that bred in them
     sf::st_sf() %>%
-    sf::st_join(dplyr::filter(breeding_data, if_any(any_of(c("year", "breeding_year")), ~. == yr))) %>%
-    #get area in new column
+    sf::st_join(dplyr::filter(breeding_data, if_any(any_of(c("year", "breeding_year")), ~ . == yr))) %>%
+    # get area in new column
     dplyr::mutate(area_polygon = sf::st_area(geometry))
 
   return(breeding_data_areas)
-
 }
 
 
 #' Get fixed effects of models
 #' #'
-#'get coef.fixed effect solution and std error for each model and put
+#' get coef.fixed effect solution and std error for each model and put
 #' into a table
 #'
-#'@param model asreml output objext
+#' @param model asreml output objext
 #'
-#'@return table with fixed effects est and se
+#' @return table with fixed effects est and se
 #'
 
 get_fixed_effects <- function(model) {
@@ -181,18 +185,18 @@ get_fixed_effects <- function(model) {
       sep = ""
     )
   )
-  #make data frame with a column with model name, and a column for coef
+  # make data frame with a column with model name, and a column for coef
   tab <- data.frame(
-    #for column called model, paste name twice
+    # for column called model, paste name twice
     model = c(deparse(substitute(model))),
-    #if there the model starts with L then trait is 'laying date'
-    #if the model starts with H then trait is 'hatching date'
+    # if there the model starts with L then trait is 'laying date'
+    # if the model starts with H then trait is 'hatching date'
     trait =
-        if (substr(deparse(substitute(model)), 1, 1) == "L") {
-      "Laying date"
-    }   else {
-      "Hatching date"
-    },
+      if (substr(deparse(substitute(model)), 1, 1) == "L") {
+        "Laying date"
+      } else {
+        "Hatching date"
+      },
     level = c("Adult", "Juvenile"),
     coef = coef
   )
@@ -204,15 +208,15 @@ get_fixed_effects <- function(model) {
 
 #' Run wald test
 #' #'
-#'Description
+#' Description
 #'
-#'@param model asreml model object
-#'@param name name of model as string
+#' @param model asreml model object
+#' @param name name of model as string
 #'
-#'@return saves an rds file with the wald output for each model
+#' @return saves an rds file with the wald output for each model
 #'
-#run wald.asreml for each model and save each output as rds file
-run_wald <- function(model, name){
+# run wald.asreml for each model and save each output as rds file
+run_wald <- function(model, name) {
   wald <- asreml::wald.asreml(model, ssType = "conditional", denDF = "numeric")
   saveRDS(wald, file = file.path(
     dirs$model_output,
